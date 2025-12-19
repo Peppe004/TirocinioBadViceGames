4f28d0e1ba2391a99c2acc662ac0703bd4f81f66
Nicoló Gioacchini <nicologioacchini@gmail.com>
2025-12-19 16:55:42 +0100

    creata classe pawn per il cart

--- Tirocinio_Giuseppe/Intermediate/Build/Win64/UnrealEditor/Inc/Tirocinio_Giuseppe/UHT/MyGameMode.generated.h
+++ Tirocinio_Giuseppe/Intermediate/Build/Win64/UnrealEditor/Inc/Tirocinio_Giuseppe/UHT/MyGameMode.generated.h
@@ -19,7 +19,7 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 // ********** Begin Class AMyGameMode **************************************************************
 TIROCINIO_GIUSEPPE_API UClass* Z_Construct_UClass_AMyGameMode_NoRegister();
 
-#define FID_Users_cerve_OneDrive_Documenti_Desktop_Tirocinio_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_INCLASS_NO_PURE_DECLS \
+#define FID_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAMyGameMode(); \
 	friend struct Z_Construct_UClass_AMyGameMode_Statics; \
@@ -30,7 +30,7 @@ public: \
 	DECLARE_SERIALIZER(AMyGameMode)
 
 
-#define FID_Users_cerve_OneDrive_Documenti_Desktop_Tirocinio_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_ENHANCED_CONSTRUCTORS \
+#define FID_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_ENHANCED_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API AMyGameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
 	/** Deleted move- and copy-constructors, should never be used */ \
@@ -42,12 +42,12 @@ public: \
 	NO_API virtual ~AMyGameMode();
 
 
-#define FID_Users_cerve_OneDrive_Documenti_Desktop_Tirocinio_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_12_PROLOG
-#define FID_Users_cerve_OneDrive_Documenti_Desktop_Tirocinio_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_GENERATED_BODY \
+#define FID_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_12_PROLOG
+#define FID_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_Users_cerve_OneDrive_Documenti_Desktop_Tirocinio_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_INCLASS_NO_PURE_DECLS \
-	FID_Users_cerve_OneDrive_Documenti_Desktop_Tirocinio_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_ENHANCED_CONSTRUCTORS \
+	FID_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_INCLASS_NO_PURE_DECLS \
+	FID_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h_15_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -57,6 +57,6 @@ class AMyGameMode;
 // ********** End Class AMyGameMode ****************************************************************
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FID_Users_cerve_OneDrive_Documenti_Desktop_Tirocinio_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h
+#define CURRENT_FILE_ID FID_TirocinioBadViceGames_Tirocinio_Giuseppe_Source_Tirocinio_Giuseppe_Public_MyGameMode_h
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Tirocinio_Giuseppe/Intermediate/Build/Win64/UnrealEditor/Inc/Tirocinio_Giuseppe/UHT/Tirocinio_Giuseppe.init.gen.cpp
+++ Tirocinio_Giuseppe/Intermediate/Build/Win64/UnrealEditor/Inc/Tirocinio_Giuseppe/UHT/Tirocinio_Giuseppe.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeTirocinio_Giuseppe_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xDA85D46F,
-				0x16FEEEC7,
+				0x8647B306,
+				0x109AD440,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Tirocinio_Giuseppe.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Tirocinio_Giuseppe.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Tirocinio_Giuseppe(Z_Construct_UPackage__Script_Tirocinio_Giuseppe, TEXT("/Script/Tirocinio_Giuseppe"), Z_Registration_Info_UPackage__Script_Tirocinio_Giuseppe, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xDA85D46F, 0x16FEEEC7));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Tirocinio_Giuseppe(Z_Construct_UPackage__Script_Tirocinio_Giuseppe, TEXT("/Script/Tirocinio_Giuseppe"), Z_Registration_Info_UPackage__Script_Tirocinio_Giuseppe, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x8647B306, 0x109AD440));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
